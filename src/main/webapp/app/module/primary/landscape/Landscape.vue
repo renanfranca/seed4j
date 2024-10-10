61ce536558384367cd188c2a9d4ea914be5f5c4f
Renan Franca <renan.andradefranca@gmail.com>
2024-10-10 12:59:06 -0300

    feat: add search functionality to landscape view

--- src/main/webapp/app/module/primary/landscape/Landscape.vue
+++ src/main/webapp/app/module/primary/landscape/Landscape.vue
@@ -1,3 +1,22 @@
 <template src="./Landscape.html"></template>
 
 <script lang="ts" src="./Landscape.component.ts"></script>
+
+<style scoped>
+.jhipster-landscape-search {
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+}
+
+.jhipster-landscape-search .jhlite-field--field {
+  flex-grow: 1;
+  margin-right: 0.5rem;
+}
+
+.jhipster-landscape-search-button {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  cursor: pointer;
+}
+</style>
