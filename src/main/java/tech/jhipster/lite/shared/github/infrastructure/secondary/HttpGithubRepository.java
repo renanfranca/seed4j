e59f77893e052b4c8571be12f705abecc75787b0
Renan Franca <renan.andradefranca@gmail.com>
2024-10-31 12:20:15 -0300

    feat: remove the feature which return the user information

--- src/main/java/tech/jhipster/lite/shared/github/infrastructure/secondary/HttpGithubRepository.java
+++ src/main/java/tech/jhipster/lite/shared/github/infrastructure/secondary/HttpGithubRepository.java
@@ -2,24 +2,20 @@ package tech.jhipster.lite.shared.github.infrastructure.secondary;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 import tech.jhipster.lite.shared.github.domain.GithubRepository;
 import tech.jhipster.lite.shared.github.domain.authentication.GithubAuthenticationCode;
 import tech.jhipster.lite.shared.github.domain.authentication.GithubToken;
-import tech.jhipster.lite.shared.github.domain.authentication.GithubUser;
 import tech.jhipster.lite.shared.github.domain.oauth.GithubOauth2Configuration;
 import tech.jhipster.lite.shared.github.infrastructure.primary.RestGithubToken;
-import tech.jhipster.lite.shared.github.infrastructure.primary.RestGithubUser;
 
 @SuppressWarnings("java:S6212")
 @Repository
 public class HttpGithubRepository implements GithubRepository {
 
   private static final String GITHUB_TOKEN_URL = "https://github.com/login/oauth/access_token";
-  private static final String GITHUB_USER_URL = "https://api.github.com/user";
 
   private final RestTemplate restTemplate;
   private final GithubOauth2Configuration configuration;
@@ -44,18 +40,6 @@ public class HttpGithubRepository implements GithubRepository {
 
     var request = new HttpEntity<>(body, headers);
     var response = restTemplate.postForEntity(GITHUB_TOKEN_URL, request, RestGithubToken.class);
-
-    return response.getBody().toDomain();
-  }
-
-  @Override
-  public GithubUser getUser(GithubToken token) {
-    HttpHeaders headers = new HttpHeaders();
-    headers.setBearerAuth(token.accessToken());
-    headers.setAccept(java.util.List.of(MediaType.APPLICATION_JSON));
-
-    var request = new HttpEntity<>(headers);
-    var response = restTemplate.exchange(GITHUB_USER_URL, HttpMethod.GET, request, RestGithubUser.class);
 
     return response.getBody().toDomain();
   }
