436fda4e6dd80eb0977d0285e57191f0fcd15d12
Renan Franca <renan.andradefranca@gmail.com>
2024-12-13 08:51:23 -0300

    feat: add GithubRepositoryVue component into ModulePropertiesFormVue component

--- src/main/webapp/app/module/primary/module-properties-form/ModulePropertiesForm.component.ts
+++ src/main/webapp/app/module/primary/module-properties-form/ModulePropertiesForm.component.ts
@@ -2,11 +2,13 @@ import { ModuleParameter } from '@/module/domain/ModuleParameter';
 import { ModuleParameterType } from '@/module/domain/ModuleParameters';
 import { ModulePropertyDefinition } from '@/module/domain/ModulePropertyDefinition';
 import { ModulePropertyKey } from '@/module/domain/ModulePropertyKey';
+import { GithubRepositoryVue } from '@/module/primary/github-repository';
 import { defineComponent, PropType } from 'vue';
 import { empty } from '../PropertyValue';
 
 export default defineComponent({
   name: 'ModulePropertiesFormVue',
+  components: { GithubRepositoryVue },
   props: {
     folderPath: {
       type: String,
